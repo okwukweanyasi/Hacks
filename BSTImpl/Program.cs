e5efc50baee223c8c2ef2dac15ffa7abb6a73654
OKWUKWE ANYASI <okwukweanyasi@users.noreply.github.com>
2020-11-13 09:29:21 +0100

    more

---
+++ BSTImpl/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BSTImpl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }      
+        
+    }
+
+       class Node
+        {
+            public Node LeftNode {get; set;}
+            public Node RightNode { get; set; }
+            public int Data { get; set; }
+    }
+}
--- BirthdayCakeCandles/Program.cs
+++ BirthdayCakeCandles/Program.cs
@@ -7,6 +7,33 @@ namespace BirthdayCakeCandles
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int[] candleArray = new int[] { 3, 2, 1, 3 };
+
+            int result = ReturnNumberOfLargest(candleArray);
+            Console.WriteLine("Largest candle: {0}", result);
+            Console.WriteLine("press any key to exit");
+            Console.ReadLine();
+
+        }
+
+        static int ReturnNumberOfLargest(int[] candles)
+        {
+            int numberOfLargest=0;
+            int largest=0;
+            for(int i=0; i < candles.Length; i++)
+            {
+                if (candles[i] > largest)
+                {
+                    largest = candles[i];
+                    numberOfLargest = 1;
+                }
+                else if (candles[i] == largest)
+                {
+                    numberOfLargest++;
+                }
+            }
+
+            return numberOfLargest;
         }
     }
 }
